932c38db93340b187ee0b3319dfffddb7ada7d1a
taga3s <bepozurapu274@gmail.com>
2024-08-08 09:25:29 +0900

    step1: setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2024: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
+  },
+};
---
+++ README.md
@@ -0,0 +1,41 @@
+# board-app
+
+## Tech stacks
+
+- React
+- TypeScript
+- vanilla-extract
+- prettier
+- eslint
+
+## Project setup
+
+1. Install deps
+
+```
+$ npm install
+```
+
+2. Copy .env.example
+```
+$ cp .env.example .env
+```
+
+## Project commands
+
+- Run app
+
+```
+$ npm run dev
+```
+
+- Lint
+
+```
+$ npm run lint
+```
+
+- Format
+```
+$ npm run format
+```
